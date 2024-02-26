69469695adedcbc6e550130588fa40ae4a8e2693
mdmdb21 <mduvbar.appventurers@gmail.com>
2024-02-26 15:05:17 +0100

    corrección errores

--- send.php
+++ send.php
@@ -2,13 +2,16 @@
 
 extract($_POST); // convertir los índices en variables de un array asociativo
 
-if (strlen(trim($nombre)) === 0 || strlen(trim($email)) === 0) {
-    echo "Error. No cubriste los campos requeridos.";
-} else {
-    if (isset($nombre)) && (isset($apellido)) && (isset($edad)) && (isset($sexo)) && (isset($email)) && (isset($phone)) {
-        echo "Hola soy $nombre $apellido! Tengo $edad años, soy $sexo, mi email es $email y mi número de teléfono es $phone";
-    }else{
-        echo "Gracias por completar el formulario.";
+if(isset($nombre)){
+
+    if (strlen(trim($nombre)) === 0 || strlen(trim($email)) === 0) {
+        echo "Error. No cubriste los campos requeridos.";
+    } else {
+        if ((isset($nombre)) && (isset($apellido)) ) {
+            echo "Hola soy $nombre $apellido! Tengo $edad años, soy $sexo, mi email es $email y mi número de teléfono es $phone";
+        }else{
+            echo "Gracias por completar el formulario.";
+        }
     }
 
 }else{
